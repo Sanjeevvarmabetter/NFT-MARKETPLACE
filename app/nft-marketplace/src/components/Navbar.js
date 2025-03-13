8a2bed14401a6ef22de0b81e9eb639c4364026f6
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2025-03-13 08:24:18 +0530

    real

--- app/nft-marketplace/src/components/Navbar.js
+++ app/nft-marketplace/src/components/Navbar.js
@@ -1,14 +1,14 @@
-import {
-    Link
-} from "react-router-dom";
-import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import '../App.css'; // Assuming the CSS file is named App.css
 
 const Navigation = ({ web3Handler, account }) => {
     return (
         <Navbar expand="lg" className="custom-navbar" variant="dark">
             <Container>
-
+                <Navbar.Brand as={Link} to="/" className="fw-bold">
+                    Decentralized RealEstate
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
@@ -23,21 +23,22 @@ const Navigation = ({ web3Handler, account }) => {
                                 href={`https://etherscan.io/address/${account}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="button nav-button btn-sm mx-4">
+                                className="button nav-button btn-sm mx-4"
+                            >
                                 <Button variant="outline-light">
                                     {account.slice(0, 5) + '...' + account.slice(38, 42)}
                                 </Button>
-
                             </Nav.Link>
                         ) : (
-                            <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                            <Button onClick={web3Handler} variant="outline-light">
+                                Connect Wallet
+                            </Button>
                         )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
-
-}
+    );
+};
 
 export default Navigation;
